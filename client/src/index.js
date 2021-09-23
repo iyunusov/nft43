df71c36512c55bb6129ca839fde4d16ffca3fd64
Islomjon Yunusov <islomjon-yunusov@mail.ru>
2021-09-24 00:42:06 +0400

    some packages added

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import { default as Root } from './modules';
+
+render(
+  <Root />,
+  document.getElementById('app')
+);
---
+++ client/src/modules/main/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      something
+    </div>
+  )
+}
+
+export default Main;
